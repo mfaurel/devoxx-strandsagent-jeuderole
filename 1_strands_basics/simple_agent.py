540272aa7b26d90894d2fe1febc4859b62738ca4
mfaurel <michael.faurel@esante.gouv.fr>
2026-04-23 14:03:03 +0200

    python

--- 1_strands_basics/simple_agent.py
+++ 1_strands_basics/simple_agent.py
@@ -1,7 +1,11 @@
+import logging
+
 from strands import Agent
 
-# TODO: Add debug logging to see what your agent is thinking
+agent = Agent()
 
-# TODO: Create the agent with the following system prompt: "You are a game master for a Dungeon & Dragon game"
+logging.basicConfig(level=logging.INFO)
 
-# TODO: Summon your agent with a basic incantation such as "Hi, I am an advanturer ready for adventure!"
+agent = Agent(system_prompt="You are a game master for a Dungeon & Dragon game")
+
+agent("Hi, I am an adventurer ready for adventure!")
